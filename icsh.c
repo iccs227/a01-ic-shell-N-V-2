2d2f5017eea135f10884d08d941204008cd6aed8
N-V-2 <jnsndvis2@gmail.com>
2025-05-16 04:20:21 +0700

    Finished milestone 2

--- icsh.c
+++ icsh.c
@@ -19,7 +19,7 @@ void toBinary(int num) {
 	}
 }
 
-void shell_interactive() { 
+int interactive_shell() { 
     char buffer[MAX_CMD_BUFFER];
     char previous_buffer[MAX_CMD_BUFFER];
     printf("Starting IC shell \n");
@@ -47,17 +47,16 @@ void shell_interactive() {
 	}
 	// 2. !!
 	if (strncmp("!!", buffer, 2) == 0){
-                printf("%s", previous_buffer);
+                printf("%s ", previous_buffer);
                 copy_buffer(previous_buffer,buffer, sizeof(buffer));
-        }
-	
+	}
 	// 1. echo
 	if (strncmp("echo ", buffer, 5) == 0){
 		int length = strlen(buffer) - 5;
 		char text[length];
 		strncpy(text, buffer+ 5, length);
 		text[length] = '\0';
-		printf("%s", text);
+		printf("%s ", text);
 	}
 	//3. exit<num>
 	else if (strncmp("exit ", buffer, 5) == 0 ) {
@@ -66,7 +65,9 @@ void shell_interactive() {
                 strncpy(text, buffer+ 5, length);
                 text[length] = '\0';
                 int num = atoi(text);
-		num = num & 0xFF;
+		if (num > 255){
+			num = num & 0xFF;
+		}
 		if(num == 1){
 			printf("Bye ~~~~\n");
 			printf(
@@ -92,12 +93,61 @@ void shell_interactive() {
     }
 }
 
+int no_interactive_shell(char buffer[], char previous_buffer[]) {
+	if (strcmp(buffer, "\n") == 0){
+                return 0;
+	}
+	if (strncmp(buffer, "#", 1) == 0){
+                return 0;
+        }
+        // 2. !!
+        if (strncmp("!!", buffer, 2) == 0){
+                no_interactive_shell(previous_buffer, buffer);
+		return 0;
+        }
+        // 1. echo
+        if (strncmp("echo ", buffer, 5) == 0){
+                int length = strlen(buffer) - 5;
+                char text[length];
+                strncpy(text, buffer+ 5, length);
+                text[length] = '\0';
+                printf("%s", text);
+		return 0;
+        }
+        //3. exit<num>
+	if (strncmp("exit ", buffer, 5) == 0 ) {
+                int length = strlen(buffer) - 5;
+                char text[length];
+                strncpy(text, buffer+ 5, length);
+                text[length] = '\0';
+                int num = atoi(text);
+		if(num > 255){
+                	num = num & 0xFF;
+		}
+                return num;
+        }
+	printf("Bad Command!!\n");
+	return 0;
+}
 
 
-int main(){
-	if(){
-		no_interactive_shell();
-	}else{
+
+int main(int argc, char *argv[]){
+	
+	if(argc >= 2){
+		FILE *file;
+		char buffer[MAX_CMD_BUFFER];
+		char previous_buffer[MAX_CMD_BUFFER];
+		int ret;
+		file = fopen(argv[1], "r");
+		while(fgets(buffer, MAX_CMD_BUFFER, file)){
+			ret = no_interactive_shell(buffer,previous_buffer);
+			copy_buffer(buffer,previous_buffer, sizeof(buffer));;
+		}
+		fclose(file);
+		return ret;
+	}
+	else{
 		interactive_shell();
 	}
 	return 0;
